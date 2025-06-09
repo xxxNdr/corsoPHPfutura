66c2bb0647dfed2760f52b1b1537d76bc271b159
xxxNdr <andrea1987andre@gmail.com>
2025-06-09 21:32:28 +0200

    Update pagine.php

--- es3/inc/pagine.php
+++ es3/inc/pagine.php
@@ -1,23 +1,6 @@
 <?php
 
-
-$prezzi = array(
-    '1' => 13,
-    '2' => 15,
-    '3' => 13,
-    '4' => 10,
-    '5' => 6.5,
-    '6' => 5
-);
-
-$piatti = array(
-    '1' => 'Ziti alla mediterranea',
-    '2' => 'Mezzelune',
-    '3' => 'Parmigiana',
-    '4' => "Carpaccio d'anguria",
-    '5' => 'Tortino al dattero',
-    '6' => 'Macedonia di stagione'
-);
+include 'inc/altre_pagine.php';
 
 $riepilogo = '';
 $totale = 0;
